Bug fix in SFRD calculation in autocalibration (I introduced it with a previous edit); finer res on SMF

autocalibration/constraints.py
@@ -39,13 +39,13 @@ GyrToYr = 1e9
 # Binning configuration
 mlow = 7
 mupp = 13
-dm = 0.2
+dm = 0.15
 mbins = np.arange(mlow, mupp, dm)
 xmf = mbins + dm/2.0
 
-mlow2 = 5
-mupp2 = 14
-dm2 = 0.3
+mlow2 = 8
+mupp2 = 12
+dm2 = 0.2
 mbins2 = np.arange(mlow2,mupp2,dm2)
 xmf2 = mbins2 + dm2/2.0
 
@@ -73,7 +73,8 @@ else:
     h0 = 0.6774
     Omega0 = 0.3089
     vol = (62.5/h0)**3 # 43 files
-    age_alist_file = '/Users/adam/MTNG_trees/MTNG_alist.txt'
+    #age_alist_file = '/Users/adam/MTNG_trees/MTNG_alist.txt'
+    age_alist_file = '/fred/oz245/MTNG/DM-Arepo/MTNG-L500-4320-A/MTNG_alist.txt'
 
 # These are two easily create variables of these different shapes without
 # actually storing a reference ourselves; we don't need it
@@ -82,6 +83,7 @@ zeros2 = lambda: np.zeros(shape=(1, 3, len(xmf2)))
 zeros3 = lambda: np.zeros(shape=(1, len(mbins)))
 zeros4 = lambda: np.empty(shape=(1), dtype=np.bool_)
 zeros5 = lambda: np.zeros(shape=(1, len(ssfrbins)))
+zeros6 = lambda: np.zeros(shape=(1, len(mbins2)))
 
 class Constraint(object):
     """Base classes for constraint objects"""
@@ -98,7 +100,7 @@ class Constraint(object):
 
         # Histograms we are interested in
         hist_smf = zeros3()
-        hist_HImf = zeros3()
+        hist_HImf = zeros6()
 
 #        fields = {
 #            'galaxies': (
@@ -141,8 +143,8 @@ class Constraint(object):
 
         logHIM = np.log10(np.sum(G['DiscHI'], axis=1)*1e10/h0)
         logHIM[~np.isfinite(logHIM)] = -20
-        hist_HImf, _ = np.histogram(logHIM, bins=mbins)
-        hist_HImf = hist_HImf / (dm * vol)
+        hist_HImf, _ = np.histogram(logHIM, bins=mbins2)
+        hist_HImf = hist_HImf / (dm2 * vol)
         
 #        # sum mass from age bins of all components
 #        StarsByAge = np.zeros(Nage)
@@ -168,7 +170,7 @@ class Constraint(object):
         TimeBinCentre = TimeBinEdge[:-1] + 0.5*dT
 #        m, lifetime, returned_mass_fraction_integrated, ncum_SN = r.return_fraction_and_SN_ChabrierIMF()
 #        eff_recycle = np.interp(TimeBinCentre, lifetime[::-1], returned_mass_fraction_integrated[::-1])
-        SFRbyAge = G['StellarFormationMass']*1e10/h0 / (dT*1e9)
+        SFRbyAge = np.sum(G['StellarFormationMass'], axis=0)*1e10/h0 / (dT*1e9)
 
 
         #########################
@@ -274,7 +276,7 @@ class SMF_z0(SMF):
 
     def get_obs_x_y_err(self):
         # Load data from Driver et al. (2022)
-        logm, logphi, dlogphi = self.load_observation('GAMA_SMF.dat', cols=[0,1,2])
+        logm, logphi, dlogphi = self.load_observation('GAMA_SMF_highres.csv', cols=[0,1,2])
         cosmology_correction_median = np.log10( r.comoving_distance(0.079, 100*h0, 0, Omega0, 1.0-Omega0) / r.comoving_distance(0.079, 70.0, 0, 0.3, 0.7) )
         cosmology_correction_maximum = np.log10( r.comoving_distance(0.1, 100*h0, 0, Omega0, 1.0-Omega0) / r.comoving_distance(0.1, 70.0, 0, 0.3, 0.7) )
         x_obs = logm + 2.0 * cosmology_correction_median 

output/plot_constraints_z0.py
@@ -1,6 +1,10 @@
 # Make plots for z=0 galaxies that are typically used to constrain Dark Sage.
 
-from pylab import *
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+import matplotlib.pyplot as plt
 import os
 import routines as r
 import random
@@ -14,13 +18,15 @@ warnings.filterwarnings("ignore")
 ###### USER NEEDS TO SET THESE THINGS ######
 #indir = '/Users/adam/DarkSage_runs/OzSTAR/mini_millennium/001/'
 #indir = '/Users/adam/DarkSage/output/results/millennium/'
-indir = '/Users/adam/DarkSage_runs/MTNG_mini/13g/' # directory where the Dark Sage data are
+indir = '/Users/adam/DarkSage_runs/MTNG_mini/13x/' # directory where the Dark Sage data are
 #indir = '/Users/adam/DarkSage_runs/Genesis/L75n324/56/'
 sim = 8 # which simulation Dark Sage has been run on -- if it's new, you will need to set its defaults below.
 #   0 = Mini Millennium, 1 = Full Millennium, 2 = SMDPL, 3 = Genesis-Millennium, 4=Genesis-Calibration, 5 = MDPL2, 6 = TNG300-1, 7 = TNG100-2, 8 = MTNG_mini
 
 fpre = 'model_z0.000' # what is the prefix name of the z=0 files
 files = range(44) # list of file numbers you want to read
+
+GAMA_dir = '/Users/adam/Documents/DarkSagePaper4/'
 
 Nannuli = 30 # number of annuli used for discs in Dark Sage
 Nage = 30 # number of age bins used for stars -- should only be used for calibrating when delayed feedback is on
@@ -128,7 +134,7 @@ try:
 #    ax[0].plot(logM_bd, SMF_bd, 'r-', lw=8, alpha=0.2, label=r'Moffett et al.~(2016) bulge-dominated', zorder=0)
     
     # new GAMA SMF, not yet public
-    data_D21 = np.loadtxt('/Users/adam/Documents/DarkSagePaper4/GAMA_SMF.txt', usecols=(0,1,2), skiprows=1, delimiter=' ', dtype=np.float64)
+    data_D21 = np.loadtxt(GAMA_dir+'GAMA_SMF.txt', usecols=(0,1,2), skiprows=1, delimiter=' ', dtype=np.float64)
     x_D21, y_D21, yerr_D21 = data_D21[:,0], data_D21[:,1], data_D21[:,2]
     y_D21 += 0.0769 # adjust for average under-density of GAMA
     y_D21 -= 3*np.log10(r.comoving_distance(0.1,100*h,0,Omega,OmegaLambda) / r.comoving_distance(0.1,70.0,0,0.3,0.7))
@@ -136,7 +142,7 @@ try:
     ax[0].errorbar(x_D21, 10**y_D21, yerr=[10**(y_D21+yerr_D21)-10**y_D21, 10**y_D21-10**(y_D21-yerr_D21)], label=r'Driver et al.~(2021)', elinewidth=2, marker='o', color='purple', linestyle='none')
     
     # new GAMA SMF by morphology, not yet public
-    data_morph = np.loadtxt('/Users/adam/Documents/DarkSagePaper4/GAMA_SMF_morph.txt', usecols=(0,1,2,7,8), skiprows=3, delimiter=' ', dtype=np.float64)
+    data_morph = np.loadtxt(GAMA_dir+'GAMA_SMF_morph.txt', usecols=(0,1,2,7,8), skiprows=3, delimiter=' ', dtype=np.float64)
     x_morph, y_Ell, yerr_Ell, y_Disc, yerr_Disc = data_morph[:,0], data_morph[:,1], data_morph[:,2], data_morph[:,3], data_morph[:,4]
     SMF_adjust = 0.0866 - 3*np.log10(r.comoving_distance(0.08,100*h,0,Omega,OmegaLambda) / r.comoving_distance(0.08,70.0,0,0.3,0.7)) # adjust for average under-density of GAMA and differnces in assumed cosmology
     y_Ell += SMF_adjust 